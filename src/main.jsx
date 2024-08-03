f4b6e6c2926cc20aa22a67a6d49f0cdacd496ba3
Meeaviishek <155694061+Meeaviishek@users.noreply.github.com>
2024-08-04 02:41:58 +0530

    change anac project

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css';
 import {RouterProvider, createBrowserRouter,Route,createRoutesFromElements} from "react-router-dom";
 import {About,Project,Services,Skills} from "./components/index.js";
+import { Analytics } from '@vercel/analytics/react';
+
 
 const router=createBrowserRouter(
   createRoutesFromElements(
@@ -20,6 +22,7 @@ const router=createBrowserRouter(
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
+    <Analytics />
    
   </React.StrictMode>,
 )
