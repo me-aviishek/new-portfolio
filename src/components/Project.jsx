a4891ff89a609b964d6edc9c1383bf80e7731696
Meeaviishek <155694061+Meeaviishek@users.noreply.github.com>
2024-08-03 23:58:27 +0530

    Initial commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -55,7 +55,22 @@ function Project() {
       <a href="https://lusi-beauty.vercel.app/" target="blank">  <button className="bg-[#1E1E1F] py-2 px-3 rounded-xl translate-all duration-700 border-[0.5px] border-[#3c3c40] shadow-md shadow-[#181818] hover:bg-[#131313]">View Site</button></a>
       </div>
     </div>
+    
+
+    <div 
+      className="bg-gradient-to-br  dark:from-[#272728] dark:to-[#323232] border border-[#353535] rounded-xl w-fit mx-auto flex flex-col justify-center gap-y-2 shadow-md shadow-[#161616] hover:scale-105 translate-all duration-300">
+      <div className="w-full flex flex-col justify-between gap-y-3 max-w-[20rem] mx-auto p-3 rounded-xl">
+        <img className="rounded-[calc(20px-12px)] rounded-b-none hover:scale-105 translate-all duration-300" loading="lazy" src={lusi} alt="Professional UI/UX Design Service" style={{width: "100%;", height:" 250px;"}} />
+        <div className="flex flex-col gap-y-1">
+          <h4 className="text-xl font-bold  lg:text-left">Influencer Engagement & Sponsorship Co-ordination Platform <h5 className="text-sm text-[#5f5f5f]">Web development</h5>
+          </h4>
+          <p className="text-black dark:text-[#c4c2c2] text-[15px] lg:text-justify">I developed an Influencer Engagement & Sponsorship Coordination Platform as part of my IITM BS degree project. This web-based application leverages Flask for backend development, SQLite for database management, Tailwind for CSS styling, Jinja for templating, and HTML for structuring content. The platform facilitates seamless interaction between influencers and sponsors, enabling efficient collaboration and coordination.</p>
+          
+          
+        </div>
   
+      </div>
+    </div>
   </motion.div>
 </div>
     </div>
